7e8ac95eae14d8261c6ecd306bebdc974e2c2db3
PostCyberPunk <lyhguest2@outlook.com>
2023-05-31 17:52:11 +0800

    feat:add command open-with-rider.open

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,18 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+import { exec } from "child_process";
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "open-with-rider" is now active!');
+	let disposable = vscode.commands.registerCommand('open-with-rider.open', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const document = editor.document;
+            const selection = editor.selection;
+            const line = selection.start.line + 1;
+            const column = selection.start.character + 1;
+            const filePath = document.fileName;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('open-with-rider.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from OpenWithRider!');
+            exec(`rider64.exe --line ${line} --column ${column} "${filePath}"`);
+        }
 	});
 
 	context.subscriptions.push(disposable);
